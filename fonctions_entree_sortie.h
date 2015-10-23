aa91fca7ca5551036eb65549102bca6a530548ec
victor2410 <victor.gaborit2@gmail.com>
2015-10-23 15:15:41 +0200

    Update fonctions_entree_sortie.h

--- fonctions_entree_sortie.h
+++ fonctions_entree_sortie.h
@@ -28,7 +28,20 @@ TMotif* Creer_Un_Motif_T1(char c, int longueur_motif);
 
 void Affiche_Dictionnaire_Motifs(TMotif* tete_motif);
 
-void Creer_Dictionnaire_Motifs_T1(TTabSeq** tab_seq, int nb_seq);
+bool existe_motif(TPMotif tete_motif, char* chaine, TPMotif* adr_prec_motif);
 
+bool existe_sequence(TPSequence tete_seq, int seq, TPSequence* adr_prec_seq);
 
+bool existe_occ(TPOccurrence tete_occ, int occ, TPOccurrence* adr_prec_occ);
 
+void Creer_Un_Motif(TPMotif* adr_tete_motif, char* chaine, TPMotif prec_motif);
+
+void Creer_Une_Sequence(TPSequence* adr_tete_seq, int seq, TPMotif* adr_tete_motif, TPSequence prec_seq);
+
+void Creer_Une_Occurrence(TOccurrence* tete_occ, int occ, TOccurrence* prec_occ);
+
+void Creer_Nouvelle_Occurrence(TPMotif* adr_tete_motif, char* chaine, int seq , int occ, bool presence_motif, bool presence_sequence, TPMotif prec_motif, TPSequence prec_seq, TPOccurrence prec_occ);
+
+bool existe(TDict dict, char* chaine, int seq, int occ, TMotif* adr_prec_motif, TSequence* adr_prec_seq, TOccurrence* adr_prec_occ, bool* adr_presence_sequence, bool* adr_presence_motif);
+
+void Creer_Dictionnaire_Motifs_T1(TDict dict, TTabSeq** tab_seq, int nb_seq);
