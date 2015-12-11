e87ba9a098f0f0ab117f5b030bafe9cb79ff96c3
victor2410 <victor.gaborit2@gmail.com>
2015-12-11 12:19:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,41 +12,102 @@
 
 /* programme principal */
 int main()
-{
-	printf("\t===================================\n");
-  
+{  
   	/* déclaration des variables */
   	int i;
   	int nb_seq; 
 	int nb_erreurs;
 	int l_max;
+	int choix;
+	int menu1, menu2;
 	float quorum;
+	char* motif_choisi;
 	FILE* fp;
-	TDict dict_vide; 
-	dict_vide.tete_motif = NULL;
-	fp = Ouvrir_Fichier();
-	nb_seq = Compte_Nb_Seq(fp);
-	printf("le nombre de sequences est : %d\n", nb_seq);
-	printf("\t===================================\n");
-	TTabSeq* tab_seq[nb_seq];
-	for (i = 0; i < nb_seq; i++)
+	TDict dict; 
+	dict.tete_motif = NULL;
+	menu1 = 1;
+	while(menu1 == 1)
 	{
-		tab_seq[i] = Alloue_TTabSeq();
+		printf("\t==========================================================================================\n");
+		printf("\t=DECOUVERTE EXACTE DE MOTIF APPROCHES AVEC ERREURS DE SUBSTITUTION, INSERTION ET DELETION=\n");
+		printf("\t==========================================================================================\n");
+		fp = Ouvrir_Fichier();
+		nb_seq = Compte_Nb_Seq(fp);
+		printf("le nombre de sequences est : %d\n", nb_seq);
+		TTabSeq* tab_seq[nb_seq];
+		for (i = 0; i < nb_seq; i++)
+		{
+			tab_seq[i] = Alloue_TTabSeq();
+		}
+		Lecture_Fichier_Sequences(fp, tab_seq);
+		/* fermeture du fichier */
+		fclose(fp);
+		menu2 = 1;
+		printf("\t============================================================\n");
+		puts("veuillez préciser la taille maximale du motif");
+		scanf("%d", &l_max);
+		while(l_max < 0)
+		{
+			puts("Taille de motif non permise, veuillez en selectionner une autre.");
+			scanf("%d", &l_max);
+		}
+		printf("\t============================================================\n");
+		puts("veuillez préciser un nombre d'erreurs maximal");
+		scanf("%d", &nb_erreurs);
+		while(nb_erreurs < 0)
+		{
+			puts("Nombre d'erreurs non permis, veuillez en selectionner un autre.");
+			scanf("%d", &nb_erreurs);
+		}
+		printf("\t============================================================\n");
+		puts("Selectionnez le quorum désiré (doit etre compris entre 0 et 100)");
+		scanf("%f", &quorum);
+		while((quorum < 0) || (quorum > 1))
+		{
+			puts("Quorum sélectionné incorrect, veuillez spécifiez un réel entre 0 et 1 (ratio).");
+			scanf("%d", &quorum);
+		}
+		printf("\t============================================================\n");
+		puts("Debut de la procédure de recherche de motifs...");
+		printf("Taille de motif recherché : %d.\n", l_max);
+		printf("Nombre d'erreurs maximum autorisées : %d.\n", nb_erreurs);
+		printf("Le quorum : %f.\n", quorum);
+		Decouvert_Exacte_Motif_Occurrences_Subst_Ins_Del(&dict, tab_seq, nb_seq, l_max, nb_erreurs, quorum);
+		puts("Procédure achevée avec succès.");
+		printf("\t============================================================\n");
+		Affiche_Dictionnaire_Motifs(dict.tete_motif, nb_seq);
+		printf("\t============================================================\n");
+		do
+		{
+			puts("1: Consulter les occurrences d'un motif donné.");
+			puts("2: Effectuer une nouvelle recherche.");
+			puts("3: Quitter le programme.");
+			scanf("%d", &choix);
+			switch(choix)
+			{
+				case 1:
+					puts("veuillez rentrer un motif à consultez dans la liste suivante:");
+					Affiche_Dictionnaire_Motifs(dict.tete_motif, nb_seq);
+					motif_choisi = Saisie_Mot();
+					puts("++++++++++++++++++++++++++++++++++++\n");
+					Affiche_Occurrences_D_Un_Motif(dict.tete_motif, motif_choisi);
+					// TODO : Affichage des occurrences selon un motif donné
+				break;
+				case 2: 
+					Destruction_dictionnaire(&dict.tete_motif);
+					dict.tete_motif = NULL;
+					menu2 = 0;
+				break;
+				case 3: 
+					puts("Fin du programme...");
+					menu2 = 0;
+					menu1 = 0;
+				break;
+				default :
+					puts("Mauvaise option selectionnée veuillez en spécifier une nouvelle.");
+			}
+		}while(menu2 == 1);
+		
 	}
-	Lecture_Fichier_Sequences(fp, tab_seq);
-	puts("veuillez préciser un nombre d'erreurs maximal");
-	scanf("%d", &nb_erreurs);
-	puts("veuillez préciser la taille maximale du motif");
-	scanf("%d", &l_max);
-	puts("Selectionnez le quorum désiré (doit etre compris entre 0 et 100)");
-	scanf("%f", &quorum);
-	//Creer_Dictionnaire_Motifs(dict_t1, tab_seq, nb_seq);
-	//Creer_Dictionnaire_Motif_vide(&dict_vide.tete_motif, tab_seq, nb_seq, nb_erreurs);
-	Decouvert_Exacte_Motif_Occurrences_Subst_Ins_Del( tab_seq, nb_seq, l_max, nb_erreurs, quorum);
-	printf("\t===================================\n");
-	
-	/* fermeture du fichier */
-	fclose(fp);
-
 	return 0;
 }
