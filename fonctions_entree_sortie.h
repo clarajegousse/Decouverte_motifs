f64c9a07f0ba7be8230ee9e950ddb4e9c7b7b729
victor2410 <victor.gaborit2@gmail.com>
2015-12-15 16:11:43 +0100

    Update fonctions_entree_sortie.h

--- fonctions_entree_sortie.h
+++ fonctions_entree_sortie.h
@@ -1,4 +1,6 @@
-/* Prototypes des fonctions de gestion d'entrée et de sortie */
+/*---Prototypes des fonctions de gestion d'entrée et de sortie---*/
+
+/*---Fonctions et Procédures de lecture de fichier---*/
 
 char* Saisie_Mot();
 
@@ -12,6 +14,16 @@ int Compte_Nb_Seq(FILE* fp);
 
 void Lecture_Fichier_Sequences(FILE* fp, TTabSeq** tab_seq);
 
+/*---Procédure de sauvegarde des résultats dans un fichier---*/
+
+void Sauvegarde_Dictionnaire_Motifs(TPMotif tete_motif, int nb_seq);
+
+/*---Fonction non spécifique à la recherche de motif---*/
+
+int minimum(int a, int b);
+
+/*---Fonctions d'allocation mémoire---*/
+
 TTabSeq* Alloue_TTabSeq();
 
 TPMotif Alloue_TMotif();
@@ -20,37 +32,41 @@ TPSequence Alloue_TSequence();
 
 TPOccurrence Alloue_TOccurrence();
 
-TMotif* Creer_Un_Motif_T1(char c, int longueur_motif);
+/*---Procédures d'affichage---*/
 
 void Affiche_Dictionnaire_Motifs(TPMotif tete_motif, int nb_seq);
 
 void Affiche_Occurrences_D_Un_Motif(TPMotif tete_motif, char* motif_recherche);
 
+/*---Fonctions et procédures de contrôle d'existance et de placement d'espion*/
+
 bool existe_motif(TPMotif tete_motif, char* chaine, TPMotif* adr_prec_motif);
 
 bool existe_sequence(TPSequence tete_seq, int seq, TPSequence* adr_prec_seq);
 
-bool existe_occ(TPOccurrence tete_occ, int occ, TPOccurrence* adr_prec_occ);
-
-void Creer_Un_Motif(TPMotif* adr_tete_motif, char* chaine, TPMotif prec_motif);
-
-void Creer_Nouvelle_Occurrence(TPMotif* adr_tete_motif, char* chaine, int seq , int occ, bool presence_motif, bool presence_sequence, TPMotif prec_motif, TPSequence prec_seq, TPOccurrence prec_occ, TPOccurrence occ_lu, int operation);
+void deplace_prec_occ(TPOccurrence tete_occ, TPOccurrence* adr_prec_occ);
 
 bool existe(TDict dict, char* chaine, int seq, int occ, TMotif* adr_prec_motif, TSequence* adr_prec_seq, TOccurrence* adr_prec_occ, bool* adr_presence_sequence, bool* adr_presence_motif);
 
-int minimum(int a, int b);
+/*---Procédures d'insertions d'éléments dans une liste---*/
+
+void Creer_Un_Motif(TPMotif* adr_tete_motif, char* chaine, TPMotif prec_motif);
 
 void Inserer_Une_Sequence(TPSequence* adr_tete_seq, int nb_seq, TPSequence prec_seq);
 
 void Inserer_Une_Occurrence(TPOccurrence* adr_tete_occ, int occ, TPOccurrence prec_occ, int last, int num_sub, int num_ins , int num_del);
 
-void deplace_prec_occ(TPOccurrence tete_occ, TPOccurrence* adr_prec_occ);
+void Creer_Nouvelle_Occurrence(TPMotif* adr_tete_motif, char* chaine, int seq , int occ, bool presence_motif, bool presence_sequence, TPMotif prec_motif, TPSequence prec_seq, TPOccurrence prec_occ, TPOccurrence occ_lu, int operation);
+
+/*---Procédures de destruction de listes et de dictionnaire---*/
 
 void Free_liste_occurrence(TPOccurrence* adr_tete_occ);
 
 void Free_liste_sequence(TPSequence* adr_tete_seq);
 
 void Destruction_dictionnaire(TPMotif* adr_tete_motif);
+
+/*---Procédures de création de dictionnaire---*/
 
 void Creer_Dictionnaire_Motif_vide(TPMotif* adr_tete_motif, TTabSeq** tab_seq, int nb_seq, int nb_erreurs);
 
